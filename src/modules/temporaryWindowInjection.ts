3cfc8d890beb8e933dad799a98c630673ed8254a
Danial <admin@redsparr0w.com>
2022-03-19 14:28:21 +1300

    Update temporaryWindowInjection.ts

--- src/modules/temporaryWindowInjection.ts
+++ src/modules/temporaryWindowInjection.ts
@@ -1,21 +1,10 @@
-// TODO: Remove temporary code after all code in ../scripts has been ported.
-// This is only here so that the code in ../scripts can use the new functionality
+// TODO: Remove temporary code in production
+// This is only here so these values can be accessed from the console
 
 import * as GameConstants from './GameConstants';
-import PokemonType from './enums/PokemonType';
-import SeededRand from './utilities/SeededRand';
-import Rand from './utilities/Rand';
-import LevelType, { levelRequirements } from './party/LevelType';
-import GenericProxy from './utilities/GenericProxy';
 import * as Settings from './utilities/Settings';
 
 Object.assign(<any>window, {
   Settings,
   GameConstants,
-  PokemonType,
-  SeededRand,
-  Rand,
-  LevelType,
-  levelRequirements,
-  GenericProxy,
 });
