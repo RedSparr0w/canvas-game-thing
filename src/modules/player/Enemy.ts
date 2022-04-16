bb6ede245e299f1b1f08c23522d5451b7e60f178
Danial <admin@redsparr0w.com>
2022-04-16 20:11:35 +1200

    Update Enemy.ts

--- src/modules/player/Enemy.ts
+++ src/modules/player/Enemy.ts
@@ -1,6 +1,7 @@
 /* eslint-disable max-classes-per-file */
 import EnemyPokemon from '../pokemons/EnemyPokemon';
 import { PokemonDirection } from '../pokemons/Pokemon';
+import Rand from '../utilities/Rand';
 import Player from './Player';
 
 export default class Enemy extends Player {
@@ -14,7 +15,7 @@ export default class Enemy extends Player {
       if (this.pokemon.length < 50) {
         this.pokemon.push(new EnemyPokemon(
           this,
-          'Rattata',
+          Rand.fromArray(['Rattata', 'Pikachu', 'Pidgey', 'Caterpie', 'Weedle', 'Mankey']),
           { x: this.map.enemy.spawn.x, y: this.map.enemy.spawn.y },
           PokemonDirection.left,
           { x: this.map.player.spawn.x, y: this.map.player.spawn.y }
