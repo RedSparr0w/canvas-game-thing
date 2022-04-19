dae4f264eb5393ea29316470e8d043e34c2fca64
Danial <admin@redsparr0w.com>
2022-04-19 14:03:48 +1200

    Update how the enemy chooses their Pokémon

--- src/modules/player/EnemyTeam.ts
+++ src/modules/player/EnemyTeam.ts
@@ -1,18 +1,35 @@
+import { MONEY_PER_TICK, MONEY_TICK } from '../GameConstants';
+import { pokemonMap } from '../pokemons/PokemonList';
+import { PokemonNameType } from '../pokemons/PokemonNameType';
 import Rand from '../utilities/Rand';
 import Team from './Team';
 
 export default class EnemyTeam extends Team {
-  spawnFrame = 0;
-  spawnFrequency = 1500;
+  spawnTick = 0;
+  nextSpawnPokemon: PokemonNameType = 'Caterpie';
+  nextSpawnTick: number = 0;
 
   draw(delta: number) {
     super.draw(delta);
 
     // Spawn a pokemon in every x ms
-    this.spawnFrame += delta;
-    if (this.spawnFrame >= this.spawnFrequency) {
-      this.spawnFrame -= this.spawnFrequency;
-      this.addPokemon(Rand.fromArray(['Rattata', 'Pikachu', 'Pidgey', 'Caterpie', 'Weedle', 'Mankey']));
+    this.spawnTick += delta;
+    if (this.spawnTick >= this.nextSpawnTick) {
+      this.addPokemon(this.nextSpawnPokemon);
+      this.nextSpawnPokemon = Rand.fromWeightedArray(
+        ['Caterpie', 'Weedle', 'Pidgey', 'Rattata', 'Pikachu', 'Mankey', 'Snorlax'],
+        [10, 10, 7, 6, 3, 3, 1]
+      );
+
+      const pokemon = pokemonMap[this.nextSpawnPokemon];
+      const moneyRequired = pokemon.cost - this.money;
+      if (!moneyRequired) {
+        // If we can afford it already, just wait some time before spawning it in
+        this.nextSpawnTick = this.spawnTick + Rand.intBetween(500, 5000);
+        return;
+      }
+      const timeToWait = Math.ceil(moneyRequired / MONEY_PER_TICK) * MONEY_TICK;
+      this.nextSpawnTick = this.spawnTick + timeToWait;
     }
   }
 }
